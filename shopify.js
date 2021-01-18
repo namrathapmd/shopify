616ca4c4eff1bb57d470dee733c8e265030737e8
Namratha <namrathapmd@gmail.com>
2021-01-17 22:48:14 -0600

    style changes

--- shopify.js
+++ shopify.js
@@ -11,17 +11,30 @@ movieName.addEventListener("keyup", function (event) {
             .then(response => response.json())
             .then(data => {
                 movies.innerHTML = data.Title !== undefined ? `
-                <li> ${data.Title} (${data.Year})
-                <button id="nominate" onclick="nominateMovie('${data.Title}','${data.Year}')">Nominate</button>
-                </li>` : `No results found for '${movieName.value}'`
+                    <div class="row">
+                        <img src="${data.Poster}" alt="movie image"
+width=200 class="search-img img-fluid">
+                    </div>
+                    <div class="row m-0 p-0 pl-1">
+                        <p class="lead m-0 p-0">${data.Title} (${data.Year})</p>
+                    </div>
+                    <div class="row m-0 p-0 pl-1">
+                        <p class="lead m-0 p-0"><small>${data.Plot}</small></p>
+                    </div>
+                    <div class="row">
+                        <button id="nominate" class="btn btn-primary"
+onclick="nominateMovie('${data.Title}','${data.Year}',
+'${data.Poster}')">Nominate</button>
+                    </div>
+                `
+                    : `No results found for '${movieName.value}'`
             });
     }
 
-}
-)
+})
 
 
-function nominateMovie(title, year) {
+function nominateMovie(title, year, poster) {
     if (count >= 5) alert("You cannot nominate more than 5 movies")
 
     else {
@@ -31,17 +44,37 @@ function nominateMovie(title, year) {
         else {
             listofmovies[title] = year
             count += 1;
-            nominations.innerHTML += `<li id="movie_${count}">${title} (${year})
-        <button onclick={deleteNominee('movie_${count}')}>X</button>
-        </li>`
+            nominations.innerHTML += `
+                <div id="movie_${count}">
+                    <div class="row m-0 p-0" >
+                        <div class="col-3">
+                            <img src="${poster}" alt="movie image"
+class="img-fluid">
+                        </div>
+                        <div class="col-7">
+                            <p class="lead m-0 p-0">
+                                <small>${title} (${year})</small>
+                            </p>
+                        </div>
+                        <div class="col-2">
+                            <button type="button" class="close"
+onclick="{deleteNominee('movie_${count}', '${title}')}"
+aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            `
         }
     }
 }
 
 
-function deleteNominee(movielistID) {
-    count--
-    let movieToRemove = document.getElementById(`${movielistID}`)
+function deleteNominee(movieListID, movieTitle) {
+    count--;
+    delete listofmovies[movieTitle];
+    let movieToRemove = document.getElementById(`${movieListID}`)
     movieToRemove.parentNode.removeChild(movieToRemove);
 }
-
